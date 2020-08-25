ddc15d8a43d4d8c9e751c9793b0c6a16b69d181e
Sean Woleslagle <jacx92@gmail.com>
2020-08-25 18:05:38 -0400

    still working through part1

--- part1/Lessons/a - Introduction to React/part1/src/index.js
+++ part1/Lessons/a - Introduction to React/part1/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
+const Hello = ({ name, age }) => {
+
+  const bornYear = () => new Date().getFullYear() - age
+  
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old.</p>
+      <p>
+        Hello {name}, you are {age} years old.
+      </p>
+      <p>
+        So you were probably born in {bornYear()}.
+      </p>
     </div>
   )
 }
@@ -12,12 +20,12 @@ const Hello = (props) => {
 const App = () => {
   const name = 'Peter'
   const age = 10
+
   return (
     <div>
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
-      <Hello />
     </div>
   )
 }
